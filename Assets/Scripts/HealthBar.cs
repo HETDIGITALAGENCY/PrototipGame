3d965521d49ed796b7581c2dd79b6925ad40fe98
ErenACR <88158555+ErenACR@users.noreply.github.com>
2024-10-09 21:05:42 +0300

    09.10.24 21;05

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    
+    public Slider HelthSlider;
+    public float maxhealth = 100f;
+    public float Health;
+   // public Slider EaseHealthSlider;
+  //  public float LerpSpeed = 0.05f;
+        
+    void Start()
+    {
+        Health = maxhealth;
+
+    }
+    
+    void Update()
+    {
+        if (HelthSlider.value != Health)
+        {
+            HelthSlider.value = Health;
+        }
+
+      //  if (HelthSlider.value != EaseHealthSlider.value)
+        //{
+          //  EaseHealthSlider.value = Math.Lerp(EaseHealthSlider.value, Health, LerpSpeed);
+        //}
+
+        void TakeDamage(float damage)
+        {
+            Health -= damage;
+        }
+        
+    }
+}
